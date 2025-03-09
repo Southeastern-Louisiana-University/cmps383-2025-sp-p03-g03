ffd5161a5536163aba145883559862aac2eb6724
Peyton <144850303+PeytPerry@users.noreply.github.com>
2025-03-09 14:13:02 -0500

    Making the suggested changes to dto files
    Adding all entities and configurations for entities.

--- Selu383.SP25.P03.Api/Features/Seats/SeatTypeDto.cs
+++ Selu383.SP25.P03.Api/Features/Seats/SeatTypeDto.cs
@@ -1,8 +1,26 @@
-﻿namespace Selu383.SP25.P03.Api.Features.Seats
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using Selu383.SP25.P03.Api.Controllers;
+using Selu383.SP25.P03.Api.Features.Movies;
+
+namespace Selu383.SP25.P03.Api.Features.Seats
 {
+    public class SeatType : IEntity
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
     public class SeatTypeDto
     {
         public int Id { get; set; }
-        public string SeatType { get; set; }
+        public string Type { get; set; }
+    }
+    public class SeatTypeConfiguration : IEntityTypeConfiguration<SeatType>
+    {
+        public void Configure(EntityTypeBuilder<SeatType> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Type).IsRequired();
+        }
     }
 }
