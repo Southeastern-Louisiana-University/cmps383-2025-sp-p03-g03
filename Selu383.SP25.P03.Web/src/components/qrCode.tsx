58b6bcc029315fec8123c37511b40fc7b2e16132
Peyton <144850303+PeytPerry@users.noreply.github.com>
2025-03-18 20:42:31 -0500

    updated qrCode componenent
    Changed this to now accept the userid as a value passed to our endpoint for getting users tickets.

--- Selu383.SP25.P03.Web/src/components/qrCode.tsx
+++ Selu383.SP25.P03.Web/src/components/qrCode.tsx
@@ -3,30 +3,21 @@
 import React, { useEffect, useState } from "react";
 import { QRCodeSVG } from "qrcode.react";
 
-export default function QRCodeComponent() {
-  //first get user data
-  //fetch movie data by id -- seat number, theater number, and showtime
-  //all of this is to be placed
+interface QRCodeComponentProps {
+  userId: number;
+}
+
+export default function QRCodeComponent({ userId }: QRCodeComponentProps) {
   const [data, setData] = useState<string>("");
-  const [user, setUser] = useState();
 
   useEffect(() => {
-    const fetchUser = async () => {
-      try {
-        const userResponse = await fetch(`https://localhost:7027/api/users`);
-        const result = await userResponse.json();
-        setUser(result.userResponse);
-      } catch (error) {
-        console.error("Error fetching user data", error);
-      }
-    };
-
     const fetchUserTickets = async () => {
       try {
-        // Replace with your actual API endpoint
-        const response = await fetch(`https://localhost:7027/api/Ticket`);
+        const response = await fetch(
+          `https://localhost:7027/api/UserTicket/GetByUserId/${userId}`
+        );
         const result = await response.json();
-        // Assume the data you want is in result.data
+
         setData(result.data);
       } catch (error) {
         console.error("Error fetching data:", error);
