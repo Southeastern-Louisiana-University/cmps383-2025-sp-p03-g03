e74326574d1f26cb39785981af577a9d38e9af78
KrakenDnD <meghan.lawrence@selu.edu>
2025-04-06 22:16:10 -0500

    build/issue

--- Selu383.SP25.P03.Mobile/utils/getLocalPoster.ts
+++ Selu383.SP25.P03.Mobile/utils/getLocalPoster.ts
@@ -15,22 +15,22 @@ export function getLocalPoster(title: string): any {
         return require('../assets/images/posters/ghostbusters.jpg');
       case 'thefallguy':
         return require('../assets/images/posters/fall-guy.jpg');
-      case 'aquietplacedayone':
-        return require('../assets/images/posters/quiet-place.jpg');
-      case 'insideout2':
-        return require('../assets/images/posters/inside-out-2.jpg');
-        case 'furiosaamadmaxsaga':
-        return require('../assets/images/posters/furiosa.jpg');
-      case 'deadpoolwolverine':
-        return require('../assets/images/posters/deadpool.jpg');
-      case 'badboysrideordie':
-        return require('../assets/images/posters/bad-boys.jpg');
-      case 'thegarfieldmovie':
-        return require('../assets/images/posters/garfield.jpg');
-      case 'oppenheimer':
-        return require('../assets/images/posters/oppenheimer.jpg');
-      default:
-        return require('../assets/images/posters/placeholder.jpg');
-    }
+    //   case 'aquietplacedayone':
+    //     return require('../assets/images/posters/quiet-place.jpg');
+    //   case 'insideout2':
+    //     return require('../assets/images/posters/inside-out-2.jpg');
+    //     case 'furiosaamadmaxsaga':
+    //     return require('../assets/images/posters/furiosa.jpg');
+    //   case 'deadpoolwolverine':
+    //     return require('../assets/images/posters/deadpool.jpg');
+    //   case 'badboysrideordie':
+    //     return require('../assets/images/posters/bad-boys.jpg');
+    //   case 'thegarfieldmovie':
+    //     return require('../assets/images/posters/garfield.jpg');
+    //   case 'oppenheimer':
+    //     return require('../assets/images/posters/oppenheimer.jpg');
+    //   default:
+    //     return require('../assets/images/posters/placeholder.jpg');
+    // }
   }
   
