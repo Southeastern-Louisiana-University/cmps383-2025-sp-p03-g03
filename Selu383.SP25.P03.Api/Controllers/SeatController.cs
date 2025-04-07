f5d120501e7d4c57ce1e494b0a170b5a91aed947
chschlich <cohen.schlicher@selu.edu>
2025-04-07 00:49:35 -0500

    backend to make it all work

--- Selu383.SP25.P03.Api/Controllers/SeatController.cs
+++ Selu383.SP25.P03.Api/Controllers/SeatController.cs
@@ -1,19 +1,35 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Selu383.SP25.P03.Api.Data;
-using Selu383.SP25.P03.Api.Features.Cart;
 using Selu383.SP25.P03.Api.Features.Seats;
 
 namespace Selu383.SP25.P03.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class SeatController : GenericController<Seat, SeatDto>
+    public class SeatController : ControllerBase
     {
+        private readonly DataContext _context;
+        private readonly IMapper _mapper;
+
         public SeatController(DataContext context, IMapper mapper)
-            : base(context, mapper)
         {
+            _context = context;
+            _mapper = mapper;
         }
 
+        // GET: api/Seat/room/5
+        [HttpGet("room/{roomId}")]
+        public async Task<ActionResult<IEnumerable<SeatDto>>> GetSeatsByRoom(int roomId)
+        {
+            var seats = await _context.Seats
+                .Where(s => s.RoomsId == roomId)
+                .OrderBy(s => s.Row)
+                .ThenBy(s => s.SeatNumber)
+                .ToListAsync();
+
+            return Ok(_mapper.Map<IEnumerable<SeatDto>>(seats));
+        }
     }
 }
